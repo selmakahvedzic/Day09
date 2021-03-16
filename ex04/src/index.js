9093e9bbc08837190c2cd6a355d1f2906951141b
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-16 15:47:12 +0100

    compareDifferentValues(m,n)

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function compareDifferentValues(m, n) {
+  if (m === n) {
+    return "Equal";
+  }
+  return "Not equal";
+}
+
+console.log(compareDifferentValues(8, "8"));
+console.log(compareDifferentValues("8", 8));
+console.log(compareDifferentValues("8", 8));
+console.log(compareDifferentValues("8", "8"));
+console.log(compareDifferentValues(8, 8));
+module.exports = compareDifferentValues;
